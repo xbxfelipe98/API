8dba2228d8f3a530ccea7251c7a2ac7dcd564e8a
Felipe Alves Pinto <falves1988@gmail.com>
2020-12-14 13:41:18 -0300

    Add files via upload

---
+++ APIFormulario/APIFormulario/Models/conn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ApiFormulario.Models
+{
+    public class conn : System.Data.Entity.DbContext
+    { 
+
+        public conn() : base("name=conn")
+        {
+
+        }
+
+        public DbSet<form> CADASTRO { get; set; }
+    }
+}
---
+++ APIFormulario/APIFormulario/Models/form.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ApiFormulario.Models
+{
+    public class form
+    {
+        [Key]
+        public Int32 ID { get; set; }
+
+        public String NOME { get; set; }
+
+        public String SOBRENOME { get; set; }
+
+        public String TELEFONE { get; set; }
+
+    }
+}
